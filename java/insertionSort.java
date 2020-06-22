1a72ed3cb178e7bd12c7d62baee9864f38e0e641
Ike Maldonado <maldonadoike@gmail.com>
2020-06-22 14:57:49 -0700

    Modify launch.json to open outside window

--- java/insertionSort.java
+++ java/insertionSort.java
@@ -1,5 +1,28 @@
 class insertionSort{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        int[] numbers = {10, 2, 78, 4, 45, 32, 7, 11};
+        System.out.println("UNSORTED: ");
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i] + " ");
+        }
+    }
+
+
+    public static int[] InsertionSort(int[] numbers){
+        int j = 0;
+        int temp = 0;
+
+        for (int i = 1; i < numbers.length; ++i){
+            j = i;
+            while (j > 0 && numbers[j] < numbers[j - 1]){
+                temp = numbers[j];
+                numbers[j] = numbers[j - 1];
+                numbers[j - 1] = temp;
+                --j; 
+            }
+        }
+        return numbers;
+
     }
 }
+
