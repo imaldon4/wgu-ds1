875c0187d58a4883ac1440167623f518cf1fcd92
Ike Maldonado <maldonadoike@gmail.com>
2020-06-23 11:32:14 -0700

    Change quicksort class to input single array argument

--- java/QuickSort.java
+++ java/QuickSort.java
@@ -1,8 +1,25 @@
 // QuickSort
 public class QuickSort{
 
-    public static void quickSort(int[] numArray, int lowIndex, int highIndex){
-        
+    // Access starts here
+    public static void quickSort(int[] array){
+        int lowIndex = 0;
+        int highIndex = array.length - 1;
+        quickSort(array, lowIndex, highIndex);
+    }
+
+    private static void quickSort(int[] numArray, int lowIndex, int highIndex){
+        if (lowIndex >= highIndex){
+            return;
+        }
+
+        int lowEndIndex = partition(numArray, lowIndex, highIndex);
+        quickSort(numArray, lowIndex, lowEndIndex);
+        quickSort(numArray, lowEndIndex + 1, highIndex);    
+    }
+
+    private static int partition(int[] numArray, int lowIndex, int highIndex) {
+        return 0;
     }
 
 }
